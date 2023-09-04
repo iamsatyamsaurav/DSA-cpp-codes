ad891c53081a0bb5b329e398923ce08482275890
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2023-09-05 01:56:00 +0530

    Add files via upload

---
+++ mergeSort.cpp
@@ -0,0 +1,43 @@
+void merge(vector <int> &arr, int low, int mid, int high){
+    vector<int>temp;
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            rig  ht++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]=temp[i-low];
+    }
+    return;
+}
+
+void mS(vector <int> &arr,int low, int high){
+    if(low>=high){
+        return;
+    }
+    int mid=low+(high-low)/2;
+    mS(arr,low,mid);
+    mS(arr,mid+1,high);
+    merge(arr,low,mid,high);
+    return;
+}
+
+void mergeSort(vector < int > & arr, int n) {
+    mS(arr,0,n-1);
+    return;
+}
