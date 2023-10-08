3678793fdd0aee8d4827b8f09026d4550a0bfd0e
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2023-10-08 18:50:45 +0530

    Add files via upload

---
+++ operatorOverloading.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    int a;
+    int b;
+    void operator+(A obj){
+        int value1=this->a;
+        int value2=obj.a;
+        cout<<"The ans is "<<value2-value1<<endl;
+    }
+    void operator()(){
+        cout<<"he he he this operator is overloaded"<<endl;
+    }
+};
+int main(){
+    A obj1,obj2;
+    obj1.a=5;
+    obj2.a=10;
+    obj1+obj2;
+    obj1();
+}
