4e8cd07b6abde3693383164b5f3cef889c97df65
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2023-10-21 23:32:40 +0530

    Add files via upload

---
+++ pointersAndFunctions.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int findSum(int arr[],int size){
+    //cout<<"Size"<<sizeof(arr)<<endl;
+
+    //cout<<*arr<<endl;
+//     int sum=0;
+//     for(int i=0;i<size;i++){
+//         sum=sum+arr[i];
+//     }
+//     return sum;
+// }
+// void print(int *p){
+//     cout<<*p<<endl;
+// }
+// void update(int *p){
+//     *p=*p+1;
+//     // cout<<p<<endl;
+// }
+int main(){
+    // int a=5;
+    // int *p=&a;
+    // // print(p);
+    // //cout<<p<<endl;
+    // cout<<*p<<endl;
+    // update(p);
+    // cout<<*p<<endl;
+
+    // int arr[6]={16,2,3,4,5,6};
+    // findSum(arr,6);
+    // int ans=findSum(arr+1,5);
+    // cout<<"The sum is \n"<<ans<<endl;
+}
