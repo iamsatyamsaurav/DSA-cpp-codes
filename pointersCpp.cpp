adf2845708b9d170ab748a8f67e1e33235ba9825
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2023-10-22 23:06:33 +0530

    Add files via upload

---
+++ pointersCpp.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int a=5;
+//     int *p=&a;
+//     // cout<<++(*p)<<" "<<p<<endl;
+//     // //cout<<*p<<endl;
+
+//    cout<<"before"<<endl;
+//    cout<<p<<endl;
+//    p=p+3;
+//    cout<<"after"<<endl;
+//    cout<<p<<endl;
+
+    // int arr[10]={2,5,6};
+    // int *p=arr;
+    // cout<<arr<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<&arr<<endl;
+
+    // cout<<p<<endl;
+    // cout<<&p<<endl;
+
+    // cout<<arr[0]<<endl;
+    // cout<<arr<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<&arr[1]<<endl;
+    // cout<<arr[1]<<endl;
+    // cout<<*(arr+1)<<endl;
+    // cout<<arr[2]<<endl;
+    // cout<<2[arr]<<endl;
+    // cout<<arr[0]<<endl;
+    // cout<<0[arr]<<endl;
+    // cout<<*arr<<endl;
+
+    // cout<<sizeof(arr)<<endl;
+
+    // int arr[10];
+    // cout<<arr<<endl;
+    // arr=arr+1; ------------> ERROR
+    // cout<<arr<<endl;
+    // int *p=arr;
+    // cout<<p<<endl;
+    // p=p+1;
+    // cout<<p<<endl; // now p is pointing to the next location
+
+    //CHARACTER ARRAY
+
+    // char ch[5]="abcd";
+    // cout<<ch<<endl;
+    // // char *c=&ch[0];
+    // cout<<c<<endl;
+    // cout<<*ch<<endl;
+    // cout<<*c<<endl;
+    // cout<<*ch<<endl;
+
+    // char *c=&ch[1];
+    // cout<<c<<endl;
+
+    // char temp='f';
+    // char *p=&temp;
+    // cout<<p<<endl;
+
+    char ch[20]="satyam saurav";
+    cout<<ch;
+      
+   
+}
