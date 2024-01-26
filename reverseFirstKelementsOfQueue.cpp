abb60f31f9029a03fd2882cef7ee0e013f92b27d
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2024-01-26 23:01:20 +0530

    Add files via upload

---
+++ reverseFirstKelementsOfQueue.cpp
@@ -0,0 +1,25 @@
+// Function to reverse first k elements of a queue.
+queue<int> modifyQueue(queue<int> q, int k) {
+    
+    stack<int>s;
+    for(int i=0;i<k;i++){
+        int element= q.front();
+        q.pop();
+        s.push(element);
+    }
+    
+    while(!s.empty()){
+        int element=s.top();
+        s.pop();
+        q.push(element);
+    }
+    
+    int t=q.size()-k;
+    while(t--){
+        int element=q.front();
+        q.pop();
+        q.push(element);
+    }
+    return q;
+    
+}
