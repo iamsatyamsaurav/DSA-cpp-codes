3fac655729ba259a7d37786c7f236a4587fbdf7f
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2024-02-15 01:42:46 +0530

    Add files via upload

---
+++ bottomViewBT.cpp
@@ -0,0 +1,27 @@
+public:
+    vector <int> bottomView(Node *root) {
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int>mp;  //hd, node
+        queue<pair<Node*, int>>q; //node, hd
+        q.push(make_pair(root,0));
+        while(!q.empty()){
+            pair<Node*,int> temp=q.front();
+            q.pop();
+            Node* frontNode=temp.first;
+            int hd=temp.second;
+            mp[hd]=frontNode->data;
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+        }
+        for(auto i:mp){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
