b0700384791b35f599984c2f37a039d504012712
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2024-04-04 19:45:43 +0530

    Add files via upload

---
+++ reverseSinglyLLInteratively.cpp
@@ -0,0 +1,46 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+        
+
+*****************************************************************/
+
+Node* reverseLinkedList(Node *head)
+{
+    if(head==NULL|| head->next==NULL){
+        return head;
+    }
+    Node* prev=NULL;
+    Node* forward=NULL;
+    Node* current=head;
+    while(current!=NULL){
+        forward=current->next;
+        current->next=prev;
+        prev=current;
+        current=forward;
+
+    }
+    return prev;
+}
