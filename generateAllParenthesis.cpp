b1194fc24f7ece8c2b4684af51d3c4ca438de886
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2024-04-18 19:36:52 +0530

    Add files via upload

---
+++ generateAllParenthesis.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+void solve(int open,int close, string op, vector<string>&v){
+    if(open==0 && close==0){
+        v.push_back(op);
+        return;
+    }
+    if(open!=0){
+        string op1=op;
+        op1.push_back('(');
+        solve(open-1,close,op1,v);
+    }
+    if(close>open){
+        string op2=op;
+        op2.push_back(')');
+        solve(open,close-1,op2,v);
+    }
+    return;
+}
+vector<string> validParenthesis(int n){
+    vector<string>v;
+    string op="";
+    int open=n;
+    int close=n;
+    solve(open,close,op,v);
+    return v;
+}
