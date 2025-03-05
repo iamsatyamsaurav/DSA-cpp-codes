ed9a0281a961593d6a9e0cd2260c694a051f97a2
Satyam Saurav <122686591+iamsatyamsaurav@users.noreply.github.com>
2025-03-05 14:44:45 +0530

    Add files via upload

---
+++ minimumElementInBST.cpp
@@ -0,0 +1,22 @@
+/*
+ * class Node
+ * {
+ * public:
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node() : data(0), left(nullptr), right(nullptr){};
+ *     Node(int x) : data(x), left(nullptr), right(nullptr) {}
+ *     Node(int x, Node *left, Node *right) : data(x), left(left), right(right) {}
+ * };
+ */
+int minVal(Node* root){
+	if(root==NULL){
+		return -1;
+	}
+	Node* temp=root;
+	while(temp->left!=NULL){
+		temp=temp->left;
+	}
+	return temp->data;	
+}
